496b9a48aa15d40a8184b98b50701cc145a134e1
Chase Ottomoeller <Chase Ottomoeller>
2016-12-20 08:04:27 -0600

    not finished

--- 2016/Problem_1.js
+++ 2016/Problem_1.js
@@ -3,98 +3,106 @@ let input = "R1, L4, L5, L5, R2, R2, L1, L1, R2, L3, R4, R3, R2, L4, L2, R5, L1,
 let letters = input.match(/[a-zA-Z]/g)
 let numbers = input.match(/\d+/g)
 
-let grid = new Array(400)
-
-for(let i = 0; i < grid.length; i++){
-    grid[i] = new Array(400)
-    for(let u = 0; u < grid.length; u++){
-        grid[i][u] = 0
-    }
-}
+let grid = new Array(200)
 
 let x = 0
 let y  = 0
 let direction = 'N'
 
 for(let n = 0; n < letters.length; n++) {
-    // console.log(x)
-    // console.log(direction)
-    // console.log(letters[n])
-    // console.log(numbers[n])
-    console.log(grid[x][y])
+    console.log(x)
+    console.log(y)
+    // console.log(grid)
+    console.log('------------------------------')
+    console.log(direction)
+    console.log(letters[n])
+    console.log(numbers[n])
+    console.log('------------------------------')
+
     /////////North////////////
     if(direction == 'N' && letters[n] == "L"){
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
+        } else {
+            console.log(" 11111111111111111111111")
+            break
+        }
         x-=parseInt(numbers[n])
         direction = 'W'
-        if(grid[x][y] == 1){
-            break
+    }
+    else if(direction == 'N' && letters[n] == "R") {
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
         } else {
-            grid[x][y] == 1
+            console.log(" 22222222222222222222222222222")
+            break
         }
-    } else if(direction == 'N' && letters[n] == "R") {
         x+=parseInt(numbers[n])
         direction = 'E'
-        if(grid[x][y] == 1){
-            break
-        } else {
-            grid[x][y] == 1
-        }
     }
     /////////South////////////
     else if(direction == 'S' && letters[n] == "L"){
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
+        } else {
+            console.log(" 333333333333333333333333333")
+            break
+        }
         x+=parseInt(numbers[n])
         direction = 'E'
-        if(grid[x][y] == 1){
-            break
+    }
+    else if(direction == 'S' && letters[n] == "R") {
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
         } else {
-            grid[x][y] == 1
+            console.log(" 444444444444444444444444444")
+            break
         }
-    } else if(direction == 'S' && letters[n] == "R") {
         x-=parseInt(numbers[n])
         direction = 'W'
-        if(grid[x][y] == 1){
-            break
-        } else {
-            grid[x][y] == 1
-        }
     }
     /////////East////////////
     else if(direction == 'E' && letters[n] == "L"){
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
+        } else {
+            console.log(" 5555555555555555555555555555")
+            break
+        }
         y+=parseInt(numbers[n])
         direction = 'N'
-        if(grid[x][y] == 1){
-            break
+    }
+    else if(direction == 'E' && letters[n] == "R") {
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
         } else {
-            grid[x][y] == 1
+            console.log(" 66666666666666666666666")
+            break
         }
-    } else if(direction == 'E' && letters[n] == "R") {
         y-=parseInt(numbers[n])
         direction = 'S'
-        if(grid[x][y] == 1){
-            break
-        } else {
-            grid[x][y] == 1
-        }
     }
     /////////West////////////
     else if(direction == 'W' && letters[n] == "L"){
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
+        } else {
+            console.log(" 77777777777777777777777777777777")
+            break
+        }
         y-=parseInt(numbers[n])
         direction = 'S'
-        if(grid[x][y] == 1){
-            break
+    }
+    else if(direction == 'W' && letters[n] == "R") {
+        if(!grid.includes(`${x} , ${y}`)){
+            grid[n] = `${x} , ${y}`
         } else {
-            grid[x][y] == 1
+            console.log(" 8888888888888888888888888888888888")
+            break
         }
-    } else if(direction == 'W' && letters[n] == "R") {
         y+=parseInt(numbers[n])
         direction = 'N'
-        if(grid[x][y] == 1){
-            break
-        } else {
-            grid[x][y] == 1
-        }
     }
 }
-
 
 console.log(Math.abs(x)+Math.abs(y))
